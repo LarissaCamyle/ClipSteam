55edf2db5d95b53489a5a5dfdbef26862a505b17
Larissa Camyle <larissacamylejob@gmail.com>
2024-09-09 18:20:54 -0300

    tratando api

--- script.js
+++ script.js
@@ -1,20 +1,103 @@
+//TRATANDO API FAKE --------------------------
 const containerVideos = document.querySelector('.container-videos')
 
-//busca na api
-const api = fetch("http://localhost:3000/videos")
-//then é quando acaba de executar a função de cima executa a 
-//função de baixo passando as info como atributo para a 
-//proxima funçã
-              //.json() transforma a resposta em json
-.then(res => res.json())
-.then((videos) => {
-    videos.forEach((video) => {
-        //acrescenta os videos printando no HTML
-        containerVideos.innerHTML += `
-            <li class="videos-item">
-                <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
-            </li>
-        `;
-    });
+//função assíncrona 
+async function buscarEMostrarVideos(){
+    try{
+        //busca na api 
+                    //espera executar a busca para prosseguir  
+        const busca = await fetch("http://localhost:3000/videos");
+        const videos = await busca.json();
+    
+        videos.forEach((video) => {
+            //acrescenta os videos printando no HTML
+            containerVideos.innerHTML += `
+                <li class="videos-item">
+                    <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+                    <div class="titulo-img-video">
+                        <img class="img-canal" src="${video.imagem} alt="Logo do Canal">
+                        <h3 class="titulo-video">${video.titulo}</h3>
+                    </div>
+                    <div class="descricao-video">
+                        <p>${video.descricao}</p>
+                        <p class="categoria-filtro" hidden> ${video.categoria}</p>
+                    </div>
+                </li>
+            `;
+        });
+    }
+    //se tiver erros
+    catch(error){
+        containerVideos.innerHTML = `<p> Houve um erro : ${error}</p>`
+    }
+    //independente ele executa
+    finally{
+
+    }
+
+}
+
+buscarEMostrarVideos();
+
+
+//BARRA DE PESQUISA ---------------------------------------
+
+const barraDePesquisa = document.querySelector('.form-pesquisa-input');
+
+barraDePesquisa.addEventListener("input", filtrarPesquisa);
+
+function filtrarPesquisa(){
+    const videos = document.querySelectorAll(".videos-item")
+
+    if(barraDePesquisa.value != ""){
+        for(let video of videos){
+            //titulo do video
+            let titulo = video.querySelector(".titulo-video").textContent.toLowerCase();
+            //barra de pesquisa
+            let valorFiltro = barraDePesquisa.value.toLowerCase();
+
+            //se o video n for oq está sendo procurado ele desaparece
+            if(!titulo.includes(valorFiltro)){
+                video.style.display = "none";
+            } 
+            else{
+                //exibe os videos
+                video.style.display = "block";
+            }
+        }
+    }
+    else{
+        for(let video of videos){
+            //se a barra de pesquisa estiver vazia
+            video.style.display = "block";
+        }
+    }
+}
+
+
+//FILTRANDO CATEGORIAS -----------------------
+
+const botaoCategoria = document.querySelectorAll(".categorias-item");
+
+botaoCategoria.forEach((botao) =>{
+    let nomeCategoria = botao.getAttribute("name");
+    botao.addEventListener("click", () => filtrarPorCategoria(nomeCategoria));
 });
 
+function filtrarPorCategoria (filtro){
+    const videos = document.querySelectorAll(".videos-item");
+
+    for(let video of videos){
+        let categoria = video.querySelector(".categoria-filtro").textContent.toLowerCase();
+        let valorFiltro = filtro.toLowerCase();
+
+        if(!categoria.includes(valorFiltro) && valorFiltro != "tudo"){
+            //se nao for da categoria selecionada ou não for "tudo"
+            video.style.display = "none";
+        }
+        else{
+            video.style.display = "block";
+        }
+
+    }
+}
